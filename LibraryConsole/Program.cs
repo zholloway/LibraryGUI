3ed5c87d6c039388eb043caf293fe195a3e4762f
Zack <zholloway92@gmail.com>
2017-04-06 20:22:35 -0400

    first commit

---
+++ LibraryConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the library GUI. Your options are as follows:");
+            Console.WriteLine("If you would like to see information about all of the books, please enter [1]");
+            Console.WriteLine("If you would like to add a new book to the library, please enter [2]");
+            var userChoice = Console.ReadLine();
+
+            if(int.Parse(userChoice) == 1)
+            {
+
+            }
+            else if (int.Parse(userChoice) == 2)
+            {
+
+            }
+        }
+    }
+}
